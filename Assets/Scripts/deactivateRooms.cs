d20ccdd44e8e5e110531ef158f5b372b1adf5844
Harrison Metcalfe <1006448@sae.edu.au>
2017-02-22 19:08:47 +1000

    Altered game flow. Rooms now spawn in with progression.

---
+++ Assets/Scripts/deactivateRooms.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deactivateRooms : MonoBehaviour
+{
+
+    public GameObject redRoom;
+    public GameObject blueRoom;
+    public GameObject purpleRoom;
+    public GameObject greenRoom;
+    public GameObject endRoom;
+    public bool endRoomIsDying = false;
+    public float timeLeft;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (endRoomIsDying)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+
+        if (timeLeft < 0)
+        {
+            endRoomIsDying = false;
+            endRoom.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        redRoom.SetActive(false);
+        blueRoom.SetActive(false);
+        greenRoom.SetActive(false);
+        purpleRoom.SetActive(false);
+        timeLeft = 5;
+        endRoomIsDying = true;
+    }
+}

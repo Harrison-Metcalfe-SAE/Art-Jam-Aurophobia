241e6bfc27b7bba786a30403d0669167974c4743
Harrison Metcalfe <1006448@sae.edu.au>
2017-02-22 16:21:40 +1000

    Added the WASD and mouse icon on start as well as title. All UI fades on movement.

---
+++ Assets/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public RawImage WASD;
+    public RawImage mouse;
+    public Text title;
+
+    public Vector3 initialMousePosition;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        if (Input.GetKey("w"))
+        {
+            WASD.CrossFadeAlpha(0, .5f, false);
+            mouse.CrossFadeAlpha(0, .5f, false);
+            title.CrossFadeAlpha(0, .5f, false);
+        }
+    }
+}
